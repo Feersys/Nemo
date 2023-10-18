d8e990b3d742ada4f1087eff4ac826f823dba0e0
arstr <ar.stramnov2004@yandex.ru>
2023-10-18 13:27:25 +0300

    add json menu

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ class Restaurant:
         self.menu = menu
 
     def reserve(self, seats):
-
+        if type(seats) != int:
+            print("Некорректное значение на вход")
+            return
         for i in self.seats_num[seats]:
             if self.tables_num[i].is_reserved is False:
                 self.tables_num[i].is_reserved = True
@@ -23,6 +25,12 @@ class Restaurant:
 
     def add_payment(self, idx, food):
         price = 0
+        if type(idx) != int:
+            print("Некорректное значение на вход")
+            return
+        if type(food) != str:
+            print("Некорректное значение на вход")
+            return
         if self.tables_num[idx].is_reserved is False:
             return
         for category in self.menu:
@@ -34,6 +42,9 @@ class Restaurant:
         print("Позиция добавлена в чек.")
 
     def payment(self, idx):
+        if type(idx) != int:
+            print("Некорректное значение на вход")
+            return
         if self.tables_num[idx].is_reserved is False:
             return
         self.total_bill += self.tables_num[idx].bill
@@ -41,6 +52,9 @@ class Restaurant:
         print("Столик оплачен. Счёт обнулён")
 
     def add_table(self, seats):
+        if type(seats) != int:
+            print("Некорректное значение на вход")
+            return
         a = Table(seats)
         idx = a.id
         if seats not in self.seats_num:
@@ -51,6 +65,9 @@ class Restaurant:
         self.tables_num[idx] = a
 
     def delete_table(self, idx):
+        if type(idx) != int:
+            print("Некорректное значение на вход")
+            return
         a = int(self.tables_num[idx].seats)
         self.seats_num[a].pop(self.seats_num[a].index(idx))
         del self.tables_num[idx]
